ce14f130fb438675598b96a0d9f80b45bcaa47a0
Faza Alhakim <fazafal04@gmail.com>
2024-08-22 16:29:16 +0700

    Client-side

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
 
 type productType = {
-  id: number;
+  id: string;
+  image: string;
   name: string;
   price: number;
-  size: string;
+  category: string;
 };
 
 const Products = () => {
@@ -19,11 +20,21 @@ const Products = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className="text-xl">Products</h1>
-      {products.map((product: productType) => (
-        <div key={product.id}>{product.name} </div>
-      ))}
+    <div className="">
+      <h1 className="text-xl font-bold text-center">Products</h1>
+      <div className="flex justify-center">
+        {products.map((product: productType) => (
+          <div key={product.id} className="basis-[20%]">
+            <img src={product.image} alt="" />
+            <h1 className="font-bold">{product.name}</h1>
+            <p className="text-[#515151]">{product.category}</p>
+            <p className="font-bold">{product.price.toLocaleString("id-ID", {
+              style: "currency",
+              currency: "IDR"
+            })}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
