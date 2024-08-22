aeb918b2426cd7cafc1eb354f679df53149eed41
Faza Alhakim <fazafal04@gmail.com>
2024-08-22 15:48:47 +0700

    API Routes

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
+type productType = {
+  id: number;
+  name: string;
+  price: number;
+  size: string;
+};
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("api/products")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.data);
+      });
+  }, []);
+
   return (
     <div>
       <h1 className="text-xl">Products</h1>
+      {products.map((product: productType) => (
+        <div key={product.id}>{product.name} </div>
+      ))}
     </div>
   );
 };
