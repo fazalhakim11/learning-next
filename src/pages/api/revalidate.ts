a62c48d849e7f64e682b8b9cdcf54e12e7246b70
Faza Alhakim <fazafal04@gmail.com>
2024-08-24 13:58:36 +0700

    Option to revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,17 +1,24 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  revalidated: boolean
-}
+  revalidated: boolean;
+  message?: string
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  if (req.query.data === "products") {
     try {
-        await res.revalidate("/products/static")
-        return res.json({revalidated: true})
+      await res.revalidate("/products/static");
+      return res.json({ revalidated: true });
     } catch (error) {
-        res.status(500).send({revalidated: false})
+      res.status(500).send({ revalidated: false });
     }
+  }
+  return res.json({
+    revalidated: false,
+    message: "Select data to revalidate!"
+  })
 }
