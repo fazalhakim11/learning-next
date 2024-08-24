eafb8640908bdf30b9cbadf2e07a6914fbb1b760
Faza Alhakim <fazafal04@gmail.com>
2024-08-24 22:44:49 +0700

    Setup Next-Auth

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,9 +1,20 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import style from "./Navbar.module.css"
 
 const Navbar = () => {
+  const { data } = useSession()
+
   return (
     <div className={style.navbar}>
       <h1>Navbar</h1>
+      <div className="flex">
+      <p className="me-5">{data && data.user?.email}</p>
+      {data ? 
+      <button onClick={()=>signOut()}>Sign Out</button>
+      :
+      <button onClick={()=>signIn()}>Sign In</button>
+      }
+      </div>
     </div>
   )
 };
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,22 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import withAuth from "./middlewares/withAuth";
 
-export const middleware = (req: NextRequest) => {
-    const isLogin = false
+// export const middleware = (req: NextRequest) => {
+//     const isLogin = true
 
-    if (isLogin){
-        return NextResponse.next()
-    }
-    return NextResponse.redirect(new URL("/auth/login", req.url))
+//     if (isLogin){
+//         return NextResponse.next()
+//     }
+//     return NextResponse.redirect(new URL("/auth/login", req.url))
+// }
+
+// export const config = {
+//     matcher: ['/products', '/products/server', '/products/static']
+// }
+
+export const authMiddleware = (req: NextRequest) => {
+    return NextResponse.next()
 }
 
-export const config = {
-    matcher: ['/products', '/products/server', '/products/static']
-}
+export default withAuth(authMiddleware, ["/products"])
