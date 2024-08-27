cd1183f7acbd35b4d12f52d819d04714c7b7cab5
Faza Alhakim <fazafal04@gmail.com>
2024-08-27 14:57:51 +0700

    Deploy

--- src/pages/products/static.tsx
+++ src/pages/products/static.tsx
@@ -13,7 +13,7 @@ const Products = (props: { products: productType[] }) => {
     <div className="">
       <h1 className="text-xl font-bold text-center">Products</h1>
       <div className="flex justify-center">
-        {products.map((product: productType) => (
+        {/* {products.map((product: productType) => (
           <div key={product.id} className="basis-[20%]">
             <img src={product.image} alt="" />
             <h1 className="font-bold">{product.name}</h1>
@@ -25,7 +25,7 @@ const Products = (props: { products: productType[] }) => {
               })}
             </p>
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
@@ -33,13 +33,13 @@ const Products = (props: { products: productType[] }) => {
 
 export default Products;
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${process.env.URL}/api/products`);
-  const response = await res.json();
-  return {
-    props: {
-      products: response.data,
-    },
-    // revalidate: 10
-  };
-};
+// export const getStaticProps = async () => {
+//   const res = await fetch(`${process.env.URL}/api/products`);
+//   const response = await res.json();
+//   return {
+//     props: {
+//       products: response.data,
+//     },
+//     // revalidate: 10
+//   };
+// };
